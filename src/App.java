f5c82cfc9beb8937eaf339ccedd23a0dba92a515
shivoqu <ivan.budiakov@student.sl.on.ca>
2022-02-05 03:43:57 -0500

    final commit

--- src/App.java
+++ src/App.java
@@ -5,54 +5,79 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> players = new ArrayList<>();
-        ArrayList<Player> playersList = new ArrayList<>(); 
+        
+        ArrayList<Player> playersList = new ArrayList<>();
+        ArrayList<Sport> sportsList = new ArrayList<>();
 
-        loadPlayerFile(players);
-        for (int i = 0; i < players.size(); i++) {
-            Player player = new Player(players.get(i));
-            playersList.add(player);
-            System.out.println(playersList.get(i).getName());
-        }
+        loadPlayerFile(sportsList, playersList);
+        
+        System.out.println("Sport :");
+            for (Sport sport : sportsList) {
+                
+                System.out.println("------------------");
+                System.out.println(sport.getName());
+                System.out.println("------------------");
+                System.out.println("Players:");
+                for(String player: sport.getPlayerList())
+                {
+                    System.out.println('\t' + player);
+                }
+            }
+            System.out.println("------------------");
+            System.out.println("All Players :");
+            for (Player player : playersList) {
+                System.out.println("\t\t" + player.getName());
+            }
+            System.out.println("*******************");
+            
     }
-        
-        
-        
-    public static /*List<String>*/ void loadPlayerFile(ArrayList<String>players) {
+
+    public static void loadPlayerFile(ArrayList<Sport> sportsList, ArrayList<Player> playersList) {
         File file = new File("res/players.txt");
-        
-        
-        try{
+        try {
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line = bufferedReader.readLine();
-            while(line != null)
-            {
-                
+            ArrayList<String> sports = new ArrayList<>();
+            // loop thru all the lines of a given file
+            while (line != null) {
+                // read and split divide the content
                 String[] lineContent = line.split(",");
-                System.out.println(line);
+                // System.out.println(line);
                 line = bufferedReader.readLine();
-                players.add(lineContent[0]);
-                
-                
+                // _________________________________
+                Player player = new Player(lineContent[0].trim());
+                playersList.add(player);
+                    // loop thru all the sports of the current line 
+                for (int i = 1; i < lineContent.length; i++) {
+                    if (sports.contains(lineContent[i].trim()))
+                        System.out.print("");
+                    else
+                    {
+                        sports.add(lineContent[i].trim());
+                        Sport sport = new Sport(lineContent[i].trim());
+                        sportsList.add(sport); 
+                        sport.addPlayer(lineContent[0].trim());
+                    }
+                    for(Sport matchingSport : sportsList)
+                    {
+                        if(lineContent[i].trim().equals(matchingSport.getName()))
+                        {
+                            matchingSport.addPlayer(lineContent[0]);
+                        }
+                    }
+                }
             }
-            System.out.println("________________________");
             bufferedReader.close();
-        }
-        catch(FileNotFoundException e)
-        {
+        } 
+        catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } 
+        catch (IOException e) {
             System.err.println(e.getMessage());
         }
-        catch(IOException e)
-        {
-            System.err.println(e.getMessage());
-        }
-        // return players;
-            
     }
-        
 
 }
--- src/Sport.java
+++ src/Sport.java
@@ -2,6 +2,19 @@ import java.util.ArrayList;
 
 public class Sport {
     String name;
-    ArrayList<Player> playerList = new ArrayList<>();
+    ArrayList<String> playerList;
 
+    public Sport(String name)
+    {
+        this.name = name;
+        playerList = new ArrayList<>();
+    }
+
+    public void addPlayer(String name)
+    {
+        playerList.add(name);
+    }
+
+    public String getName() {return name;}
+    public ArrayList<String> getPlayerList() {return playerList;}
 }
